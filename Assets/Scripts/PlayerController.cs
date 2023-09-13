fdd3508fb97f8d622c0384841d09d396d1891ffb
Hashi <rerenaga9170@icloud.com>
2023-09-13 19:37:38 +0900

    Player's movement with boundary
    My first if statements for user's movement horizontally

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float speed = 10;
+    private float xrange = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        //Playerのx座標を取得して、それが0未満ないし10を超えたらそれぞれ処理をする
+        if(transform.position.x > xrange)
+        {
+            transform.position = new Vector3(xrange, transform.position.y, transform.position.z);
+        }else if(transform.position.x < -xrange)
+        {
+            transform.position = new Vector3(-xrange, transform.position.y, transform.position.z);
+        }
+    }
+}
